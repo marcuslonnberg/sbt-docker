8e5304a26e8ae198d6290420a86a22896d09aad1
Marcus Lönnberg <marcus@lonnberg.nu>
2014-07-19 23:23:55 +0200

    Add examples with immutable Dockerfile

--- examples/DockerfileExamples.scala
+++ examples/DockerfileExamples.scala
@@ -1,7 +1,53 @@
-import sbtdocker.Dockerfile
-import sbtdocker.Instructions._
+import java.io.File
 
-// Sequence of instructions
+import sbtdocker.Instructions._
+import sbtdocker._
+
+// There is both a mutable and an immutable Dockerfile.
+// Both share the same API where all Dockerfile instructions have a corresponding method and it keeps track of files
+// that should be copied to the staging directory.
+
+val jarFile: File = ???
+
+// An immutable Dockerfile
+
+val base = immutable.Dockerfile.empty
+  .from("ubuntu")
+  .run("apt-get", "-y", "install", "openjdk-7-jre-headless")
+  .add(jarFile, "/srv/app.jar")
+  .workDir("/srv")
+  .cmd("java", "-jar", "app.jar")
+
+// A mutable Dockerfile (which does the same as the immutable example)
+
+new mutable.Dockerfile {
+  from("ubuntu")
+  run("apt-get", "-y", "install", "openjdk-7-jre-headless")
+  add(jarFile, "/srv/app.jar")
+  workDir("/srv")
+  cmd("java", "-jar", "app.jar")
+}
+
+// Benefits of the mutable Dockerfile is that it is easy to conditionally include instructions and adding repeated
+// instructions with values from a collection.
+
+val numbers = List(1, 1, 2, 3, 5, 8)
+val earthIsRound = true
+
+new mutable.Dockerfile {
+  from("ubuntu")
+
+  if (earthIsRound) {
+    expose(80)
+  }
+
+  numbers foreach { n =>
+    run("echo", n.toString)
+  }
+}
+
+// A Dockerfile can also be created with a sequence of instructions and a sequence of files that should exist in the
+// stage dir.
 
 val instructions = Seq(
   From("ubuntu"),
@@ -10,29 +56,8 @@ val instructions = Seq(
   Cmd("java", "-jar", "app.jar")
 )
 
-val dockerfile1 = Dockerfile(instructions)
+val stagedFiles = Seq(
+  CopyPath(new File("/path/to/app.jar"), new File("app.jar"))
+)
 
-// Mutable Dockerfile
-
-val dockerfile2 = Dockerfile()
-dockerfile2.from("ubuntu")
-dockerfile2.run("apt-get", "-y", "install", "openjdk-7-jre-headless")
-dockerfile2.add("app.jar", "app.jar")
-// Conditionally include instructions
-if (true) {
-  dockerfile2.cmd("java", "-jar", "app.jar")
-}
-
-
-val dockerfile3 = new Dockerfile {
-  from("ubuntu")
-  run("apt-get", "-y", "install", "openjdk-7-jre-headless")
-  add("app.jar", "app.jar")
-  // Conditionally include instructions
-  if (true) {
-    cmd("java", "-jar", "app.jar")
-  }
-}
-
-// They are all the same
-dockerfile1 == dockerfile2 && dockerfile2 == dockerfile3
+Dockerfile(instructions, stagedFiles)
