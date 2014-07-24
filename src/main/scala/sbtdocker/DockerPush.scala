16b2d7c4a8c67124bae146dc63598ebd8506d072
Marcus Lönnberg <marcus@lonnberg.nu>
2014-07-24 16:57:21 +0200

    Fix documentation

--- src/main/scala/sbtdocker/DockerPush.scala
+++ src/main/scala/sbtdocker/DockerPush.scala
@@ -1,26 +1,27 @@
 package sbtdocker
 
 import sbt._
+
 import scala.sys.process.{Process, ProcessLogger}
 
 object DockerPush {
   /**
-    * Push to a registry or docker hub
-    *
-    * @param imageName name of the resulting image
-    * @param stageDir stage dir
-    * @param log logger
-    */
+   * Push an image to a registry or docker hub
+   *
+   * @param dockerPath path to the docker binary
+   * @param imageName name of the image to push
+   * @param log logger
+   */
   def apply(dockerPath: String, imageName: ImageName, log: Logger) {
-    log.info(s"Pushing docker image with name: '${imageName.name}'")
+    log.info(s"Pushing docker image with name: '$imageName'")
 
     val processLog = ProcessLogger({ line =>
-                                     log.info(line)
-                                   }, { line =>
-                                     log.info(line)
-                                   })
+      log.info(line)
+    }, { line =>
+      log.info(line)
+    })
 
-    val command = dockerPath :: "push" :: imageName.name :: Nil
+    val command = dockerPath :: "push" :: imageName.toString :: Nil
     log.debug(s"Running command: '${command.mkString(" ")}'")
 
     val processOutput = Process(command).lines(processLog)
