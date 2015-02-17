cd4784bd87776b482b717e55f8124cf372edae9b
Marcus Lönnberg <marcus@lonnberg.nu>
2015-02-17 21:13:49 +0100

    Add tests for StagedDockerfile

---
+++ src/test/scala/sbtdocker/StagedDockerfileSpec.scala
@@ -0,0 +1,53 @@
+package sbtdocker
+
+import org.scalatest.{FlatSpec, Matchers}
+import sbt._
+import sbtdocker.Instructions.{Expose, From, Maintainer}
+
+class StagedDockerfileSpec extends FlatSpec with Matchers {
+  "A staged Dockerfile" should "be empty from the start" in {
+    val sdf = StagedDockerfile.empty
+    sdf.instructions shouldBe empty
+    sdf.stageFiles shouldBe empty
+  }
+
+  it should "immutable" in {
+    val sdf = StagedDockerfile.empty
+    sdf.addInstruction(From("abc"))
+    sdf.stageFile(CopyFile(file("s1")), file("d1"))
+
+    sdf.instructions shouldBe empty
+    sdf.stageFiles shouldBe empty
+  }
+
+  it should "contain stage files" in {
+    val sdf = StagedDockerfile.empty
+      .stageFile(CopyFile(file("s1")), file("d1"))
+      .stageFile(CopyFile(file("s2")), file("d2"))
+
+    sdf.stageFiles should contain theSameElementsAs Seq(
+      CopyFile(file("s1")) -> file("d1"),
+      CopyFile(file("s2")) -> file("d2")
+    )
+  }
+
+  it should "contain instructions" in {
+    val sdf = StagedDockerfile.empty
+      .addInstruction(From("abc"))
+      .addInstruction(Maintainer("xyz"))
+
+    sdf.instructions should contain theSameElementsInOrderAs Seq(
+      From("abc"),
+      Maintainer("xyz")
+    )
+  }
+
+  it should "generate a instructions string" in {
+    val sdf = StagedDockerfile.empty
+      .addInstruction(From("abc"))
+      .addInstruction(Maintainer("xyz"))
+      .addInstruction(Expose(Seq(80)))
+
+    sdf.instructionsString shouldEqual "FROM abc\nMAINTAINER xyz\nEXPOSE 80"
+  }
+}
