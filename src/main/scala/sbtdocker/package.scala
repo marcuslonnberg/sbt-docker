64a69d8f4415443c3dd9b8c7b14d9432be9ac693
Marcus Lönnberg <marcus@lonnberg.nu>
2015-02-21 21:29:14 +0100

    Clarify deprecation message

--- src/main/scala/sbtdocker/package.scala
+++ src/main/scala/sbtdocker/package.scala
@@ -2,8 +2,8 @@ package object sbtdocker {
   val Dockerfile = mutable.Dockerfile
   type Dockerfile = mutable.Dockerfile
 
-  @deprecated("Use instruction with same name instead", "0.6.0")
+  @deprecated("Use sbtdocker.Instructions.StageFile", "0.6.0")
   type StageFile = Instructions.StageFile
-  @deprecated("Use instruction with same name instead", "0.6.0")
+  @deprecated("Use sbtdocker.Instructions.StageFile", "0.6.0")
   val StageFile = Instructions.StageFile
 }
