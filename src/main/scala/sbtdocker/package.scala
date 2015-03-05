cb1771e6ac4848f6f0211b43035feb8f8255ffab
Marcus Lönnberg <marcus@lonnberg.nu>
2015-03-05 21:37:34 +0100

    Add ImmutableDockerfile as alias for immutable.Dockerfile

--- src/main/scala/sbtdocker/package.scala
+++ src/main/scala/sbtdocker/package.scala
@@ -2,6 +2,9 @@ package object sbtdocker {
   val Dockerfile = mutable.Dockerfile
   type Dockerfile = mutable.Dockerfile
 
+  type ImmutableDockerfile = immutable.Dockerfile
+  val ImmutableDockerfile = immutable.Dockerfile
+
   @deprecated("Use sbtdocker.Instructions.StageFiles", "0.6.0")
   type StageFile = Instructions.StageFiles
   @deprecated("Use sbtdocker.Instructions.StageFiles", "0.6.0")
