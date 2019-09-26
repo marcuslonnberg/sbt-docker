cdc77ad2637589b495198e193c2ac7a58da6074d
Simon Johansson <simon.johansson@minatjanster.se>
2019-09-26 08:33:23 +0200

    fix sanitation test

--- src/test/scala/sbtdocker/ImageNameSpec.scala
+++ src/test/scala/sbtdocker/ImageNameSpec.scala
@@ -89,6 +89,6 @@ class ImageNameSpec extends FlatSpec with Matchers {
   }
 
   it should "sanitize bad tags" in {
-    ImageName(repository = "repo", tag = Some("än/Unu5u4|-tag")).toString shouldEqual "repo:-n-Unu5u4--tag"
+    ImageName("repo:än/Unu5u4|-tag").toString shouldEqual "repo:-n-Unu5u4--tag"
   }
 }
